e5ba462fdd847dc1958eede523d2efba6d0585d8
Yasir Irshad <yasirirshad208@gmail.com>
2025-08-01 18:27:51 +0500

    fhg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://use.typekit.net/nfp1ryg.css" />
+      </head>
       <body
       >
         <Navbar/>
